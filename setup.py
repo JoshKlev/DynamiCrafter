32a00d4acf71ae8f3067997bfb97267fb1cd5498
JoshKlev <85079328+JoshKlev@users.noreply.github.com>
2024-07-06 03:47:09 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='DynamiCrafter',
+    version='1.0',
+    packages=find_packages(),
+    install_requires=[
+    ],
+    author='',
+    author_email='',
+    description='',
+    url='https://github.com/JoshKlev/DynamiCrafter',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
