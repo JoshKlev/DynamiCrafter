89fee2316c8a6b7e778a60c099d78e7f1aaf99a1
JoshKlev <85079328+JoshKlev@users.noreply.github.com>
2024-07-06 04:58:26 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='DynamiCrafter',
     version='0.1',
     package_dir={'': '.'},
-    packages=["lvdm"],
+    packages=["lvdm", "utils"],
     install_requires=[
         # List your project's dependencies here.
         # For example: 'requests', 'numpy'
